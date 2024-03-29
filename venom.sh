4b13a9969ca572683ccf0f48f2a88917ba472e3d
Nayanblack <130187443+Nayanblack@users.noreply.github.com>
2024-03-29 15:08:31 -0300

    Add files via upload

---
+++ venom.sh
@@ -0,0 +1,11 @@
+clear
+LIGHTPURPLE='\033[1;35m'
+while :
+do
+echo "${LIGHTPURPLE}
+             VenomBase - md _ Auto reconexão ativada 
+                    para prevenção de quedas.."
+       
+    node .
+sleep 1      
+done
